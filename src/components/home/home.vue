5730fada3624bf3209afb1c38af59b9f88bb9b18
helloyucom <wsy89@sina.cn>
2020-03-09 15:29:10 +0800

    头部样式调整

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,31 +1,48 @@
 <template>
-<el-container class="container">
-  <el-header class="header">Header</el-header>
-  <el-container>
-    <el-aside class="aside" width="200px">Aside</el-aside>
-    <el-main class="main">Main</el-main>
+  <el-container class="container">
+    <el-header class="header">
+      <el-row>
+        <el-col :span="23" class="middle">
+          <h2>电商后台管理系统</h2>
+        </el-col>
+        <el-col :span="1">
+          <a href="#" class="loginOut">退出</a>
+        </el-col>
+      </el-row>
+    </el-header>
+    <el-container>
+      <el-aside class="aside" width="200px">Aside</el-aside>
+      <el-main class="main">Main</el-main>
+    </el-container>
   </el-container>
-</el-container>
 </template>
 <script>
 export default {
-   data() {
-      return {
-      }
-   }
-}
+  data() {
+    return {};
+  }
+};
 </script>
 <style>
 .container {
-    height: 100%;
+  height: 100%;
 }
 .header {
-    background-color: #b3c0d1;
+  background-color: #b3c0d1;
 }
 .aside {
-    background-color: #d3dce6;
+  background-color: #d3dce6;
 }
 .main {
-    background-color: #e9eef3;
+  background-color: #e9eef3;
+}
+/** 头部样式 */
+.middle {
+    text-align: center;
+    line-height: 100%;
+}
+.loginOut {
+    text-decoration: none;
+    line-height: 60px;
 }
 </style>
