420bb96937901d98cde7641f5a944c71fb8ed2ec
helloyucom <wsy89@sina.cn>
2020-03-09 21:52:31 +0800

    用户列表-分页数据渲染

--- src/components/users/users.vue
+++ src/components/users/users.vue
@@ -77,13 +77,13 @@ export default {
   methods: {
     /** 分页 页码变化 每页条数变化 */
     handleSizeChange(val) {
-      console.log(`每页 ${val} 条`);
+      // console.log(`每页 ${val} 条`);
       this.pagesize = val;
       this.pagenum = 1;
       this.getUserList();
     },
     handleCurrentChange(val) {
-      console.log(`当前页: ${val}`);
+      // console.log(`当前页: ${val}`);
       this.pagenum = val;
       this.getUserList();
     },
