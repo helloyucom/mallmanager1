04769554ccf3b3836f61d6713edcb61741032157
helloyucom <wsy89@sina.cn>
2020-03-09 14:16:30 +0800

    新建登录分支新建登录组件配置路由

--- src/main.js
+++ src/main.js
@@ -9,6 +9,17 @@ import ElementUI from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css'
 Vue.use(ElementUI)
 
+// 不要忘记引入css文件
+import '@/assets/css/reset.css'
+
+// axios以前的写法，现在想Vue.use这种形式使用
+/* import axios from 'axios'
+Vue.proototype.$http = axios; */
+
+import MyServerHttp from '@/plugins/http.js'
+Vue.use(MyServerHttp)
+
+
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
