90dd4b1578cdfca5eff11feaa39912f7dd867d22
helloyucom <wsy89@sina.cn>
2020-03-09 15:14:08 +0800

    home中引入布局容器

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -1,7 +1,11 @@
 <template>
-    <div>
-        home
-    </div>
+<el-container class="container">
+  <el-header class="header">Header</el-header>
+  <el-container>
+    <el-aside class="aside" width="200px">Aside</el-aside>
+    <el-main class="main">Main</el-main>
+  </el-container>
+</el-container>
 </template>
 <script>
 export default {
@@ -12,4 +16,16 @@ export default {
 }
 </script>
 <style>
+.container {
+    height: 100%;
+}
+.header {
+    background-color: #b3c0d1;
+}
+.aside {
+    background-color: #d3dce6;
+}
+.main {
+    background-color: #e9eef3;
+}
 </style>
