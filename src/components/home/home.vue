9854f0b2e9aa29ebfc37d3c017253109ce10e3db
helloyucom <wsy89@sina.cn>
2020-03-09 16:52:13 +0800

    用户路由配置

--- src/components/home/home.vue
+++ src/components/home/home.vue
@@ -12,8 +12,10 @@
     </el-header>
     <el-container>
       <el-aside class="aside" width="200px">
+        <!-- 开启路由功能 -->
         <el-menu
         class="menue"
+        :router="true"
         :unique-opened="true"
         >
           <!-- 1 -->
@@ -22,7 +24,7 @@
               <i class="el-icon-user-solid"></i>
               <span>用户管理</span>
             </template>
-            <el-menu-item index="1-1">
+            <el-menu-item index="users">
               <i class="el-icon-user"></i>
               <span slot="title">用户列表</span>
             </el-menu-item>
@@ -85,7 +87,9 @@
           </el-submenu>
         </el-menu>
       </el-aside>
-      <el-main class="main">Main</el-main>
+      <el-main class="main">
+          <router-view></router-view>
+      </el-main>
     </el-container>
   </el-container>
 </template>
--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import Router from 'vue-router'
 // import Login from '../components/login/login.vue'
 import Login from '@/components/login/login.vue'
 import Home from '@/components/home/home.vue'
+import Users from '@/components/Users/users.vue'
 
 Vue.use(Router)
 
@@ -16,7 +17,10 @@ export default new Router({
     {
       name: 'home',
       path: "/",
-      component: Home
+      component: Home,
+      children: [
+        {name: 'users', path: '/users', component: Users}
+      ]
     }
   ]
 })
