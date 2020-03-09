eb8b804ca8cc377326ed141415e76986b3e0e5fb
helloyucom <wsy89@sina.cn>
2020-03-09 15:03:00 +0800

    登录成功，保存token

--- src/components/login/login.vue
+++ src/components/login/login.vue
@@ -31,6 +31,8 @@ export default {
       } = res.data;
       if (status === 200) {
         // 登录成功
+        // 0. 保存token
+        localStorage.setItem('token', data.token)
         // 1. 跳转到home
         this.$router.push({ name: "home" });
         // 2. 提示成功
