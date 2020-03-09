2155e3545d2bf649939b56b8ff44f82435fba622
helloyucom <wsy89@sina.cn>
2020-03-09 17:15:49 +0800

    用户列表卡片-面包屑-搜索框-btn按钮

--- src/components/users/users.vue
+++ src/components/users/users.vue
@@ -1,13 +1,43 @@
 <template>
-<div>用户</div>
+  <el-card class="box-card">
+    <!-- 面包屑 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <!-- <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item> -->
+      <el-breadcrumb-item>首页</el-breadcrumb-item>
+      <el-breadcrumb-item>用户管理</el-breadcrumb-item>
+      <el-breadcrumb-item>用户列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!-- 搜索 -->
+    <el-row class="searchRow">
+      <el-col>
+        <el-input placeholder="请输入内容" v-model="query" class="inputSearch">
+          <el-button slot="append" icon="el-icon-search"></el-button>
+        </el-input>
+        <el-button type="success" plain>添加用户</el-button>
+      </el-col>
+    </el-row>
+    <!-- 表格 -->
+
+    <!-- 分页 -->
+  </el-card>
 </template>
 <script>
 export default {
-   data() {
-      return {
-      }
-   }
-}
+  data() {
+    return {
+      query: ""
+    };
+  }
+};
 </script>
 <style>
+.box-card {
+   height: 100%;
+}
+.searchRow {
+   margin-top: 20px;
+}
+.inputSearch {
+   width: 300px;
+}
 </style>
