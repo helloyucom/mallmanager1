07651d8bf1b9ef147422b4bf696ff96c51a1ddad
helloyucom <wsy89@sina.cn>
2020-03-12 11:44:22 +0800

    商品管理-商品列表-商品添加-面包屑-提示-tabs

--- src/components/goods/goodsAdd.vue
+++ src/components/goods/goodsAdd.vue
@@ -1,13 +1,37 @@
 <template>
-<div>商品添加</div>
+  <el-card class="card-box">
+    <!-- 面包屑 -->
+    <my-bread level1="商品管理" level2="商品列表"></my-bread>
+    <!-- 提示 -->
+    <el-alert title="添加商品信息" type="success" center show-icon></el-alert>
+    <!-- 步骤 -->
+    <el-steps :active="parseInt(active)" finish-status="success" simple style="margin-top: 20px">
+      <el-step title="基本信息"></el-step>
+      <el-step title="商品参数"></el-step>
+      <el-step title="商品属性"></el-step>
+      <el-step title="商品图片"></el-step>
+      <el-step title="商品内容"></el-step>
+    </el-steps>
+    <!-- tabs便签页 -->
+    <el-tabs v-model="active" tab-position="left" style="height: 400px;">
+      <el-tab-pane name="1" label="基本信息">基本信息</el-tab-pane>
+      <el-tab-pane name="2" label="商品参数">商品参数</el-tab-pane>
+      <el-tab-pane name="3" label="商品属性">商品属性</el-tab-pane>
+      <el-tab-pane name="4" label="商品图片">商品图片</el-tab-pane>
+      <el-tab-pane name="5" label="商品内容">商品内容</el-tab-pane>
+    </el-tabs>
+  </el-card>
 </template>
 <script>
 export default {
-   data() {
-      return {
-      }
-   }
-}
+  data() {
+    return {
+        active: '1'
+    };
+  }
+};
 </script>
 <style>
+.card-box {
+}
 </style>
