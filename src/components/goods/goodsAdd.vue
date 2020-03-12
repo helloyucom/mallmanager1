26c66c7a3c731748d63840edbd8e8abff0142ec7
helloyucom <wsy89@sina.cn>
2020-03-12 12:12:21 +0800

    商品管理-商品列表-商品添加-基本信息表单展示

--- src/components/goods/goodsAdd.vue
+++ src/components/goods/goodsAdd.vue
@@ -13,20 +13,57 @@
       <el-step title="商品内容"></el-step>
     </el-steps>
     <!-- tabs便签页 -->
-    <el-tabs v-model="active" tab-position="left" style="height: 400px;">
-      <el-tab-pane name="1" label="基本信息">基本信息</el-tab-pane>
-      <el-tab-pane name="2" label="商品参数">商品参数</el-tab-pane>
-      <el-tab-pane name="3" label="商品属性">商品属性</el-tab-pane>
-      <el-tab-pane name="4" label="商品图片">商品图片</el-tab-pane>
-      <el-tab-pane name="5" label="商品内容">商品内容</el-tab-pane>
-    </el-tabs>
+    <el-form label-position="top" label-width="80px" :model="form"  style="height: 400px; overflow: auto;">
+      <el-tabs v-model="active" tab-position="left">
+        <el-tab-pane name="1" label="基本信息">
+          <el-form-item label="商品名称">
+            <el-input v-model="form.goods_name"></el-input>
+          </el-form-item>
+          <el-form-item label="商品价格">
+            <el-input v-model="form.goods_price"></el-input>
+          </el-form-item>
+          <el-form-item label="商品重量">
+            <el-input v-model="form.goods_weight"></el-input>
+          </el-form-item>
+          <el-form-item label="商品数量">
+            <el-input v-model="form.goods_number"></el-input>
+          </el-form-item>
+        </el-tab-pane>
+        <el-tab-pane name="2" label="商品参数">商品参数</el-tab-pane>
+        <el-tab-pane name="3" label="商品属性">商品属性</el-tab-pane>
+        <el-tab-pane name="4" label="商品图片">商品图片</el-tab-pane>
+        <el-tab-pane name="5" label="商品内容">商品内容</el-tab-pane>
+      </el-tabs>
+    </el-form>
+
+    <!-- <el-form label-position="top" label-width="80px" :model="form">
+      <el-form-item label="名称">
+        <el-input v-model="formLabelAlign.name"></el-input>
+      </el-form-item>
+      <el-form-item label="活动区域">
+        <el-input v-model="formLabelAlign.region"></el-input>
+      </el-form-item>
+      <el-form-item label="活动形式">
+        <el-input v-model="formLabelAlign.type"></el-input>
+      </el-form-item>
+    </el-form>-->
   </el-card>
 </template>
 <script>
 export default {
   data() {
     return {
-        active: '1'
+      active: "1",
+      form: {
+        goods_name: "",
+        goods_price: "",
+        goods_weight: "",
+        goods_number: "",
+        goods_cat: "",
+        goods_introduce: "",
+        pics: "",
+        attrs: ""
+      }
     };
   }
 };
